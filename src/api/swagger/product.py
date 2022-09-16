ccbc23f69b36b5f50136aae534e27318131f66ce
vandersvanlin <vandersvanlin@gmail.com>
2022-09-16 19:35:52 +0300

    minor fix

--- src/api/swagger/product.py
+++ src/api/swagger/product.py
@@ -42,7 +42,7 @@ class ProductOutputGetAll:
     summary: Optional[str] = 'Get all products by parameters'
     description: Optional[str] = (
         "**Returns** all products from db by **parameters**.<br />"
-        "Available to all **.registered users**"
+        "Available to all **registered users**"
     )
     response_model: Optional[Type[Any]] = list[ProductGetSchema]
     status_code: Optional[int] = status.HTTP_200_OK
@@ -54,7 +54,7 @@ class ProductOutputGet:
     summary: Optional[str] = 'Get product by product id'
     description: Optional[str] = (
         "**Returns** product from db by **product id**.<br />"
-        "Available to all **.registered users**"
+        "Available to all **registered users**"
     )
     response_model: Optional[Type[Any]] = ProductGetSchema
     status_code: Optional[int] = status.HTTP_200_OK
