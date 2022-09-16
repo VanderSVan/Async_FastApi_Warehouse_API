f498f0107bbb91616934222bb2ba2929c59d0eee
vandersvanlin <vandersvanlin@gmail.com>
2022-09-16 19:15:29 +0300

    Prepare db for tests

--- src/db/db_sqlalchemy.py
+++ src/db/db_sqlalchemy.py
@@ -1,14 +1,20 @@
+from sys import modules
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 from src.config import get_settings
 
-setting = get_settings()
+settings = get_settings()
 
-URL = setting.get_database_url()
-engine = create_async_engine(URL, future=True, echo=True)
+if "pytest" in modules:
+    URL = settings.get_test_database_url()
+else:
+    URL = settings.get_database_url()
 
-async_session = sessionmaker(expire_on_commit=False, class_=AsyncSession, bind=engine)
+async_engine = create_async_engine(URL, future=True, echo=True)
+
+async_session = sessionmaker(expire_on_commit=False, class_=AsyncSession, bind=async_engine)
 
 BaseModel = declarative_base()
