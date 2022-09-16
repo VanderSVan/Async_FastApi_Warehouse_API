835f73aab2961924a7211def5db7d64c47f812a2
vandersvanlin <vandersvanlin@gmail.com>
2022-09-16 19:16:43 +0300

    Add tests for `user` endpoint

---
+++ tests/functional_tests/test_data.py
@@ -0,0 +1,31 @@
+# !!!!!!!!! DON'T CHANGE THESE DATA !!!!!!!!!
+
+users_json: list = [
+    {
+        "username": "admin",
+        "email": "admin@example.com",
+        "phone": "0123456789",
+        "role": "admin",
+        "id": 1,
+        "status": "confirmed",
+        "password": "123456789"
+    },
+    {
+        "username": "client1",
+        "email": "client1@example.com",
+        "phone": "147852369",
+        "role": "user",
+        "id": 2,
+        "status": "confirmed",
+        "password": "123654789"
+    },
+    {
+        "username": "client2",
+        "email": "client2@example.com",
+        "phone": "1478523690",
+        "role": "user",
+        "id": 3,
+        "status": "unconfirmed",
+        "password": "123654789"
+    }
+]
